4d9ad3155a5dc4dcd7bb720d06b24a48345fc6b7
Calvin <calvinsoong@cmail.carleton.ca>
2019-11-04 14:23:38 -0500

    Add express and basic socket setup

--- WebServer/app.js
+++ WebServer/app.js
@@ -1,14 +1,24 @@
 const http = require('http');
+const express = require('express');
+const net = require('net');
 
 const hostname = '127.0.0.1';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Server running at http://${hostname}:${port}/`));
+
+const IFS = net.connect(1010, 'localhost');
+IFS.write('Test msg from node');
+IFS.end();
