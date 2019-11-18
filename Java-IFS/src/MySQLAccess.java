095ef9d4e279222c8b72547d9bd7954ea8b5e822
Ryan <ryanzheng@cmail.carleton.ca>
2019-11-17 23:26:06 -0500

    Added connection to database using jdbc

---
+++ Java-IFS/src/MySQLAccess.java
@@ -0,0 +1,96 @@
+// Adapted from http://www.vogella.com/tutorials/MySQLJava/article.html
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+public class MySQLAccess {
+	
+  private Connection connect = null;
+  private Statement statement = null;
+  private PreparedStatement preparedStatement = null;
+  private ResultSet resultSet = null;
+
+  final private String url = "jdbc:mysql://localhost:3306/sysc-4907";
+  final private String user = "root";
+  final private String passwd = "sysc4907";
+  
+  public void readDataBase() throws Exception {
+    try {
+      // This will load the MySQL driver, each DB has its own driver
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      
+      
+      // Setup the connection with the DB
+      connect = DriverManager.getConnection(url, user, passwd);
+
+      // Statements allow to issue SQL queries to the database
+      statement = connect.createStatement();
+      // Result set get the result of the SQL query
+      resultSet = statement.executeQuery("select * from patient_info");
+      writeMetaData(resultSet);
+      resultSet = statement.executeQuery("select * from patient_medical_info");
+      writeMetaData(resultSet);
+    } catch (Exception e) {
+      throw e;
+    } finally {
+      close();
+    }
+
+  }
+
+  private void writeMetaData(ResultSet resultSet) throws SQLException {
+    //   Now get some metadata from the database
+    // Result set get the result of the SQL query
+    
+    System.out.println("The columns in the table are: ");
+    
+    System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
+    for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
+      System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
+    }
+  }
+
+  private void writeResultSet(ResultSet resultSet) throws SQLException {
+    // ResultSet is initially before the first data set
+    while (resultSet.next()) {
+      // It is possible to get the columns via name
+      // also possible to get the columns via the column number
+      // which starts at 1
+      // e.g. resultSet.getSTring(2);
+      String user = resultSet.getString("myuser");
+      String website = resultSet.getString("webpage");
+      String summary = resultSet.getString("summary");
+      Date date = resultSet.getDate("datum");
+      String comment = resultSet.getString("comments");
+      System.out.println("User: " + user);
+      System.out.println("Website: " + website);
+      System.out.println("Summary: " + summary);
+      System.out.println("Date: " + date);
+      System.out.println("Comment: " + comment);
+    }
+  }
+
+  // You need to close the resultSet
+  private void close() {
+    try {
+      if (resultSet != null) {
+        resultSet.close();
+      }
+
+      if (statement != null) {
+        statement.close();
+      }
+
+      if (connect != null) {
+        connect.close();
+      }
+    } catch (Exception e) {
+
+    }
+  }
+
+}
---
+++ Java-IFS/src/StartUp.java
@@ -0,0 +1,10 @@
+
+public class StartUp {
+
+	public static void main(String[] args) throws Exception {
+		MySQLAccess connection = new MySQLAccess();
+		connection.readDataBase();
+
+	}
+
+}
